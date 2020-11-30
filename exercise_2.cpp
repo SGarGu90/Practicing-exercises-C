e38acc7ac14453342400d7bfb42f84f32f54a53a
SGarGu90 <sgargu90@gmail.com>
2020-11-30 19:54:49 +0100

    Add loop for each row and add empty spaces on start for each row

--- exercise_2.cpp
+++ exercise_2.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include <string>
+#include<iomanip>
 using namespace std;
 
 #include "helpers.cpp"
 
+
+void print_empty_start_characters_in_row(int total_start_empty_characters_in_row, int EMPTY_WIDTH) {
+    for (int empty_char_slot = 0; empty_char_slot < total_start_empty_characters_in_row; empty_char_slot++) {
+        cout << setw(EMPTY_WIDTH) << " ";
+    }
+}
+
 int main() 
 {
+    int EMPTY_WIDTH = 2;
     int total_number_rows;
+    cout << "Enter number of rows: ";
     total_number_rows = cin_number();
+    cout << "----------------------\n";
 
-    for (int i = 0; i < total_number_rows; i++)
-    {
-        cout << i;
+    for(int row_number = 0; row_number < total_number_rows; row_number++) {
+        int total_start_empty_characters_in_row = total_number_rows-row_number-1;
     }
-    
+
 
     return 0;
 }
