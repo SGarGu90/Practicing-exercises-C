03a1e7d76f8cba54a16d7705624a3d5e1f26250e
SGarGu90 <sgargu90@gmail.com>
2020-12-02 01:16:37 +0100

    Add function to save user name and user age within valodator of age

--- exercise_3.cpp
+++ exercise_3.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 #include "helpers.cpp"
 
-int MAX_TOTAL_USER_STORAGE = 50;
+int MAX_TOTAL_USER_ON_MEMORY = 50;
+int MIN_AGE_ALLOWED = 0;
+int MAX_AGE_ALLOWED = 125;
 
 string calc_dni()
 {
@@ -64,6 +66,39 @@ void load_menu()
     cout << "\t0: EXIT\n"; 
 }
 
+int save_user_gender()
+{
+    
+}
+
+int validate_age(int age)
+{
+    if (age > 0 || age < 125) return true;
+    else cout << "'" << age << "'" << "is not valid range. (0 - 125)\n"; 
+}
+
+int save_user_age()
+{
+    int age;
+    bool is_not_correct_age = true;
+    cout << "Edad ";
+    do {
+        age = cin_number();
+        is_not_correct_age = validate_age(age);
+    } while (is_not_correct_age);
+    
+    return age;
+}
+
+string save_user_name()
+{
+    string user_name;
+    cout << "Nombre: ";
+    cin >> user_name;
+
+    return user_name;
+}
+
 int main() 
 {
     int selected_option_id;
@@ -71,12 +106,12 @@ int main()
     int total_users;
     string operation_name;
 
-    string all_user_name[MAX_TOTAL_USER_STORAGE];
-    int all_user_age[MAX_TOTAL_USER_STORAGE];
-    char all_user_gender[MAX_TOTAL_USER_STORAGE];
-    string all_user_marital_status[MAX_TOTAL_USER_STORAGE];
-    float all_user_weight[MAX_TOTAL_USER_STORAGE];
-    float all_user_height[MAX_TOTAL_USER_STORAGE];
+    string all_user_name[MAX_TOTAL_USER_ON_MEMORY];
+    int all_user_age[MAX_TOTAL_USER_ON_MEMORY];
+    char all_user_gender[MAX_TOTAL_USER_ON_MEMORY];
+    string all_user_marital_status[MAX_TOTAL_USER_ON_MEMORY];
+    float all_user_weight[MAX_TOTAL_USER_ON_MEMORY];
+    float all_user_height[MAX_TOTAL_USER_ON_MEMORY];
 
     cout << "How many people to insert? ";
     total_users = cin_number();
@@ -84,11 +119,9 @@ int main()
     for (int user_position = 0; user_position < total_users; user_position++) {
         cout << "Introduzca los datos del usuario " << user_position + 1 << "\n";
 
-        cout << "Nombre: ";
-        cin >> all_user_name[user_position];
-
-        cout << "Edad ";
-        all_user_age[user_position] = cin_number();
+        all_user_name[user_position] = save_user_name();
+        all_user_age[user_position] = save_user_age();
+        all_user_gender[user_position] = save_user_gender();
 
         cout << "Gender:\n \t - (m) male\n \t - (f) female\n";
         cin >> all_user_gender[user_position];
@@ -104,16 +137,18 @@ int main()
     }
 
 
-    do {
-        load_menu();
-        selected_option_id = cin_number();
-        operation_name = get_operation_name_by_id(selected_option_id);
-        is_valid_operation_option = operation_name != "undefined";
+    // do {
+    //     load_menu();
+    //     selected_option_id = cin_number();
+    //     operation_name = get_operation_name_by_id(selected_option_id);
+    //     is_valid_operation_option = operation_name != "undefined";
 
-        if (is_valid_operation_option) execute(operation_name); // TBD users
-        else cout << "Program exited correctly.\n";
+    //     // Select user of list to apply operation
+    //     if (is_valid_operation_option) execute(operation_name);
+
+    //     else cout << "Program exited correctly.\n";
         
-    } while (is_valid_operation_option);
+    // } while (is_valid_operation_option);
     
 
     return 0;
