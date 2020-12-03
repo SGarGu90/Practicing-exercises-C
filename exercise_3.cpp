55024c26873260120105abb97e1c5e11671e3094
SGarGu90 <sgargu90@gmail.com>
2020-12-03 06:14:15 +0100

    Fix some small errors and improve pompt styles. Continue development check gender method

--- exercise_3.cpp
+++ exercise_3.cpp
@@ -24,29 +24,52 @@ string calc_dni(int user_id)
     cout << "calc_dni";
 }
 
-bool is_gender(int user_id)
+
+char request_user_gender()
 {
-    cout << "is_gender";
+    // char gender;
+    // bool is_correct_gender = false;
+    // cout << "Gender:\n " << setw(MENU_INDENT_WIDTH) << " - (m) male\n " << setw(MENU_INDENT_WIDTH) << " - (f) female\n";
+    // do {
+    //     cin >> gender;
+    //     is_correct_gender = (gender == 'm' || gender == 'f');
+    //     if (!is_correct_gender) cout << "Gender '" << gender << "'" << " is not valid.\n " << setw(MENU_INDENT_WIDTH) << " - (m) male\n " << setw(MENU_INDENT_WIDTH) << " - (f) female\n";
+    // } while (!is_correct_gender);
+
+    // return gender;
 }
 
-bool is_adult(int age)
+bool validate_gender(int user_id)
+{
+    cout << "Type gender to check user (" << user_id << ") " << all_user_name[user_id] << "\n";
+    char to_match_gender = request_user_gender();
+
+    cout << "Gender is ";
+    if (to_match_gender == all_user_gender[user_id]) cout << "correct";
+    else cout << "NOT correct, set new gender '";
+    cout << "for user '" << all_user_name[user_id] << "'\n";
+    
+    all_user_gender[user_id] = request_user_gender();
+}
+
+bool check_if_adult(int age)
 {
     if (age > 18) cout << "Is adult";
     else cout << "Is young";
 }
 
-float calc_imc(float height, float weight)
+float calc_bmi(float height, float weight)
 {
-    float imc = (weight / ( int(height) ^ 2 )) / 100;
-    cout << "(IMC): ";
-    if (imc < 20) cout << "Debajo de su peso: ";
-    else if (imc >= 21 && imc <= 25) cout << "Peso ideal: ";
-    else if (imc > 25) cout << "Sobrepeso: ";
-    else cout << "It has not been possible to calculate IMC. Check 'weight' nor 'height' of user";
-    cout << imc << " (kg/m)";
+    float bmi = (weight / ( int(height) ^ 2 )) / 100;
+    cout << "(BMI): ";
+    if (bmi < 20) cout << "Debajo de su peso: ";
+    else if (bmi >= 21 && bmi <= 25) cout << "Peso ideal: ";
+    else if (bmi > 25) cout << "Sobrepeso: ";
+    else cout << "It has not been possible to calculate BMI. Check 'weight' nor 'height' of user";
+    cout << bmi << " (kg/m)\n";
 }
 
-int get_highest_name_length()
+int get_longest_name_length()
 {
     int count;
     int longest_name = all_user_name[0].size();
@@ -58,19 +81,19 @@ int get_highest_name_length()
 
 void list_users()
 {
+    int MINIMUM_RIGHT_SPACING = 9; // Used name in list less than menu header "name     |", persist minimum empty spaces for right prompt style in name list
     string user_name;
     int total_users = MAX_TOTAL_USER_ON_MEMORY;
-    int highet_name_length = get_highest_name_length();
-    cout << "Gender: (m) male | (f) female\n";
-    cout << "Marital status: (s) single | (m) married | (d) divorced | (w) widowed\n";
+    int longest_name_length = get_longest_name_length();
+    cout << " - (id) name     | age | gender | marital status | weight (kg) | height (cm)\n";
     cout << "------------------------------------------------------------------------\n";
-    cout << " - (id) name | age | gender | marital status | weight (kg) | height (cm)\n";
-    cout << "------------------------------------------------------------------------\n";
+    int right_empty_spaces;
     for (int user_pos = 0; user_pos < total_users; user_pos++) {
         user_name = all_user_name[user_pos];
         if (all_user_name[user_pos] == "") break;
+        longest_name_length = (longest_name_length > MINIMUM_RIGHT_SPACING) ? longest_name_length : MINIMUM_RIGHT_SPACING;
+        right_empty_spaces = (longest_name_length - user_name.size());
 
-        int right_empty_spaces = highet_name_length - user_name.size();
         if (right_empty_spaces != 0 ) right_empty_spaces += 1;
         cout << " - (" << user_pos << ") " << all_user_name[user_pos] << setw(right_empty_spaces) << " ";
         cout << "|  " << all_user_age[user_pos] <<  "  |  "
@@ -90,36 +113,37 @@ int select_user()
     return selected_user_id;
 }
 
+string get_operation_title_by_name(string operation_name)
+{
+    if (operation_name == "bmi") return "Body Mass Index";
+    else if (operation_name == "isadult") return "Is adult?";
+    else if (operation_name == "isgender") return "Is gender ..";
+    else if (operation_name == "newdni") return "New DNI number";
+}
+
 int execute(string operation_name)
 {
+    cout << "\n\t>>>>> '" << get_operation_title_by_name(operation_name) << "' operation <<<<< \n\n";
     int selected_user_id = select_user();
+
     string user_name = all_user_name[selected_user_id];
     int user_age = all_user_age[selected_user_id];
     char user_gender = all_user_gender[selected_user_id];
     float user_height = all_user_height[selected_user_id];
     float user_weight = all_user_weight[selected_user_id];
 
-    cout << "\n'" << operation_name << "' operation result: \n";
-    cout << "User " << "'" << user_name << "' ";
-    
-    if (operation_name == "imc") cout << calc_imc(user_height, user_weight);
-    else if (operation_name == "isadult") is_adult(user_age);
-    else if (operation_name == "isgender") is_gender(user_gender);
+    cout << "\n'" << get_operation_title_by_name(operation_name) << "' operation result: \n";
+    //cout << "User " << "'" << user_name << "' ";
+    if (operation_name == "bmi") cout << calc_bmi(user_height, user_weight);
+    else if (operation_name == "isadult") check_if_adult(user_age);
+    else if (operation_name == "isgender") validate_gender(user_gender);
     else if (operation_name == "newdni") calc_dni(selected_user_id);
     cout << "_______________________________________________\n";
 }
 
-string get_operation_title_by_name(string operation_name)
-{
-    if (operation_name == "imc") return "Indice de masa corporal";
-    else if (operation_name == "isadult") return "¿Es adulto?";
-    else if (operation_name == "isgender") return "Is gender?";
-    else if (operation_name == "newdni") return "New DNI number";
-}
-
 string get_operation_name_by_id(int operation_id)
 {
-    return (operation_id == 1) ? "imc" : 
+    return (operation_id == 1) ? "bmi" : 
             (operation_id == 2) ? "isadult" : 
             (operation_id == 3) ? "isgender" : 
             (operation_id == 4) ? "newdni" : "undefined";
@@ -138,102 +162,63 @@ void load_menu()
     cout << setw(MENU_INDENT_WIDTH) << " " << "0: EXIT\n";
 }
 
-bool is_valid_height(float height)
-{
-    return (height > 9 && height <= 250);
-}
-
-float save_user_height()
+float request_user_height()
 {
     float height;
     bool is_correct_height = false;
     cout << "Height (10 - 250) cm: ";
     do {
         height = cin_number_float();
-        is_correct_height = is_valid_height(height);
+        is_correct_height = (height > 9 && height <= 250);
         if (!is_correct_height) cout << "Height '" << height << "'" << " is not valid range.\nEnter valid range(10 - 250) cm\n";
     } while (!is_correct_height);
     
     return height;  
 }
 
-bool is_valid_weight(float weight)
-{
-    return (weight > 0 && weight <= 1000);
-}
-
-float save_user_weight()
+float request_user_weight()
 {
     float weight;
     bool is_correct_weight = false;
     cout << "Weight (0.1 - 1000) kg: ";
     do {
         weight = cin_number_float();
-        is_correct_weight = is_valid_weight(weight);
+        is_correct_weight = (weight > 0 && weight <= 1000);
         if (!is_correct_weight) cout << "Weight '" << weight << "'" << " is not valid range.\nEnter valid range(0.1 - 1000) kg\n";
     } while (!is_correct_weight);
     
     return weight;  
 }
 
-bool is_valid_marital_status(char marital_status)
-{
-    return (marital_status == 's' || marital_status == 'm' || marital_status == 'd' || marital_status == 'w');
-}
-
-char save_user_marital_status()
+char request_user_marital_status()
 {
     char marital_status;
     bool is_correct_marital_status = false;
-    cout << "Marital status:\n \t - (s) single\n \t - (m) married\n \t - (d) divorced\n \t - (w) widowed\n";
+    cout << "Marital status:\n " << setw(MENU_INDENT_WIDTH) << " - (s) single\n " << setw(MENU_INDENT_WIDTH) << " - (m) married\n " << setw(MENU_INDENT_WIDTH) << " - (d) divorced\n " << setw(MENU_INDENT_WIDTH) << " - (w) widowed\n";
     do {
         cin >> marital_status;
-        is_correct_marital_status = is_valid_marital_status(marital_status);
-        if (!is_correct_marital_status) cout << "Marital status '" << marital_status << "'" << " is not valid.\n \t - (s) single\n \t - (m) married\n \t - (d) divorced\n \t - (w) widowed\n";
+        is_correct_marital_status = (marital_status == 's' || marital_status == 'm' || marital_status == 'd' || marital_status == 'w');
+        if (!is_correct_marital_status) cout << "Marital status '" << marital_status << "'" << " is not valid.\n " << setw(MENU_INDENT_WIDTH) << " - (s) single\n " << setw(MENU_INDENT_WIDTH) << " - (m) married\n " << setw(MENU_INDENT_WIDTH) << " - (d) divorced\n " << setw(MENU_INDENT_WIDTH) << " - (w) widowed\n";
     } while (!is_correct_marital_status);
     
     return marital_status;
 }
 
-bool is_valid_gender(char gender)
-{
-    return (gender == 'm' || gender == 'f');
-}
-
-char save_user_gender()
-{
-    char gender;
-    bool is_correct_gender = false;
-    cout << "Gender:\n \t - (m) male\n \t - (f) female\n";
-    do {
-        cin >> gender;
-        is_correct_gender = is_valid_gender(gender);
-        if (!is_correct_gender) cout << "Gender '" << gender << "'" << " is not valid.\n \t - (m) male\n \t - (f) female\n";
-    } while (!is_correct_gender);
-    
-    return gender;
-}
-
-bool is_valid_age(int age)
-{
-    return (age >= 1 && age <= 125);
-}
-
-int save_user_age()
+int request_user_age()
 {
     int age;
     bool is_correct_age = false;
     cout << "Age (1 - 125): ";
     do {
         age = cin_number_int();
-        is_correct_age = is_valid_age(age);
+        is_correct_age = (age >= 1 && age <= 125);
         if (!is_correct_age) cout << "Age '" << age << "'" << " is not valid range.\nEnter valid range(1 - 125)\n";
     } while (!is_correct_age);
     
     return age;
 }
 
-string save_user_name()
+string request_user_name()
 {
     string user_name = "-";
     cout << "Name: ";
@@ -251,14 +236,14 @@ void save_users_data()
 
     for (int user_position = 0; user_position < users_to_insert; user_position++) {
         cout << "Insert data user nº " << user_position + 1 << "\n";
-        all_user_name[user_position] = save_user_name();
-        all_user_age[user_position] = save_user_age();
-        //all_user_gender[user_position] = save_user_gender();
-        //all_user_marital_status[user_position] = save_user_marital_status();
-        //all_user_weight[user_position] = save_user_weight();
-        //all_user_height[user_position] = save_user_height();
+        all_user_name[user_position] = request_user_name();
+        //all_user_age[user_position] = request_user_age();
+        all_user_gender[user_position] = request_user_gender();
+        //all_user_marital_status[user_position] = request_user_marital_status();
+        //all_user_weight[user_position] = request_user_weight();
+        //all_user_height[user_position] = request_user_height();
         cout << "------------------------------\n";
-        cout << "------------------------------\n";
+        cout << "\n";
     }
 
     cout << "Users saved sucessfully\n";
