dfaba557da408dee65e6068c6140fb095c603b56
SGarGu90 <sgargu90@gmail.com>
2020-12-05 06:53:38 +0100

    Create functions to request day and month specifically to validate input numbers and add extra prompts from process

--- exercise_4.cpp
+++ exercise_4.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <string>
 #include <limits> // To request strict number in cin (not allowed letters) numeric_limits<streamsize>..
-// #include <iomanip> // use set() to generate empty spaces between text
-// #include <math.h>
-// #include <sstream> // string to int 
 
 using namespace std;
 
@@ -18,56 +15,89 @@ int cin_number_int()
     return input;
 }
 
-int * request_input_day_month_year(int validate_year = 0)
+int request_day()
+{
+    int day;
+    cout << "\tday: ";
+    bool is_correct_day;
+    do {
+        day = cin_number_int();
+        is_correct_day = (day > 0  && day < 32);
+        if (!is_correct_day) cout << "Expected must be higher or equal birth year, please introduce valid year," << endl; 
+        if (!is_correct_day) cout << "Day '" << day << "' is not valid. Please insert correct day range(1-31)"  << endl; 
+    } while (!is_correct_day);
+    return day;
+}
+
+int request_month()
+{
+    cout << "\tmonth: ";
+    int month;
+    bool is_correct_month;
+    do {
+        month = cin_number_int();
+        is_correct_month = (month > 0  && month < 13);
+        if (!is_correct_month) cout << "Month '" << month << "' is not valid. Please insert correct month range(1-12)"  << endl; 
+    } while (!is_correct_month);
+    return month;
+}
+
+int * request_date_to_calc_age(int validate_year = 0)
 {
     static int day_month_year[3];
-    cout << "\tday: ";
-    day_month_year[0] = cin_number_int();
-
-    cout << "\tmonth: ";
-    day_month_year[1] = cin_number_int();
-
+    day_month_year[0] = request_day();
+    day_month_year[1] = request_month();
     cout << "\tyear: ";
     if (validate_year > 0) {
         bool is_correct_year;
-        do
-        {
+        do {
             day_month_year[2] = cin_number_int();
             is_correct_year = (day_month_year[2] >= validate_year);
             if (!is_correct_year) cout << "Expected must be higher or equal birth year, please introduce valid year," << endl; 
         } while (!is_correct_year);
     }
-    else day_month_year[2] = cin_number_int();
 
     return day_month_year;
 }
 
+int * request_birth_date(int validate_year = 0)
+{
+    static int day_month_year[3];
+    day_month_year[0] = request_day();
+    day_month_year[1] = request_month();
+    cout << "\tyear: ";
+    day_month_year[2] = cin_number_int();
+
+    return day_month_year;
+}
+
+int calc_age(int * user_birth_date, int * to_expect_date)
+{
+    int age;
+    age = to_expect_date[2] - user_birth_date[2];
+    cout << age << endl;
+    if (to_expect_date[1] >= user_birth_date[1] && to_expect_date[0] >= user_birth_date[0]) age++;
+    return age;
+}
+
 int main()               
 { 
-    int *user_birth_date;
-    int *to_expect_date;
-    
+    cout << "Negative years are accepted and represent XXXX(b.C) dates.";
     cout << "Introduce your birth date: " << endl;
     cout << "> Birth: " << endl;
-    user_birth_date = request_input_day_month_year();
-    int birth_day = user_birth_date[0];
-    int birth_month = user_birth_date[1];
-    int birth_year = user_birth_date[2];
+    int *user_birth_date = request_birth_date();
+    cout << "Birth date: " << user_birth_date[0] << "/" << user_birth_date[1] << "/" << user_birth_date[2] << endl;
     cout << "\n ----------------------" << endl;
     cout << "Introduce date to calculate age: " << endl;
     cout << "> Expect age at: " << endl;
-    to_expect_date = request_input_day_month_year(birth_year);
-    int expect_day = user_birth_date[0];
-    int expect_month = user_birth_date[1];
-    int expect_year = user_birth_date[2];
+    int *to_expect_date = request_date_to_calc_age(user_birth_date[2]);
+    cout << "Expect date: " << to_expect_date[0] << "/" << to_expect_date[1] << "/" << to_expect_date[2] << endl;
 
-    int age;
-    age = expect_year - birth_year;
-    if (expect_month >= birth_month && expect_day >= birth_day) age++;
-    
+    int age = calc_age(user_birth_date, to_expect_date);
+
     cout << endl;
-    cout << "Birth date: " << birth_day << "/" << birth_month << "/" << birth_year << endl;
-    cout << "Expect date: " << expect_day << "/" << expect_month << "/" << expect_year << endl;
+    cout << "Birth date: " << user_birth_date[0] << "/" << user_birth_date[1] << "/" << user_birth_date[2] << endl;
+    cout << "Expect date: " << to_expect_date[0] << "/" << to_expect_date[1] << "/" << to_expect_date[2] << endl;
     cout << "Age is: " << age << endl;
     cout << endl;
 
