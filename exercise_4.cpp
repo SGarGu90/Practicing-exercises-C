9c48479f6770e7f284f177f346e002e210804e67
SGarGu90 <sgargu90@gmail.com>
2020-12-05 12:25:45 +0100

    Fix calc diff date by passed input user age

--- exercise_4.cpp
+++ exercise_4.cpp
@@ -44,61 +44,81 @@ int request_month()
 
 int * request_date_to_calc_age(int validate_year = 0)
 {
-    static int day_month_year[3];
-    day_month_year[0] = request_day();
-    day_month_year[1] = request_month();
+    static int month_year[2];
+    month_year[0] = request_month();
     cout << "\tyear: ";
     if (validate_year > 0) {
         bool is_correct_year;
         do {
-            day_month_year[2] = cin_number_int();
-            is_correct_year = (day_month_year[2] >= validate_year);
+            month_year[1] = cin_number_int();
+            is_correct_year = (month_year[1] >= validate_year);
             if (!is_correct_year) cout << "Expected must be higher or equal birth year, please introduce valid year," << endl; 
         } while (!is_correct_year);
     }
 
-    return day_month_year;
+    return month_year;
 }
 
-int * request_birth_date(int validate_year = 0)
+int * request_start_date(int validate_year = 0)
 {
-    static int day_month_year[3];
-    day_month_year[0] = request_day();
-    day_month_year[1] = request_month();
+    static int month_year[2];
+    month_year[0] = request_month();
     cout << "\tyear: ";
-    day_month_year[2] = cin_number_int();
+    month_year[1] = cin_number_int();
 
-    return day_month_year;
+    return month_year;
 }
 
-int calc_age(int * user_birth_date, int * to_expect_date)
+int calc_month_diff(int * start_user_date, int * end_user_date)
+{
+    int month_diff;
+    if (start_user_date[0] >= end_user_date[0]) month_diff = end_user_date[0];
+    else month_diff = end_user_date[0] - start_user_date[0];    
+
+    return month_diff;
+}
+
+int calc_age_diff(int * start_user_date, int * end_user_date)
 {
     int age;
-    age = to_expect_date[2] - user_birth_date[2];
+    age = end_user_date[1] - start_user_date[1] -1;
     cout << age << endl;
-    if (to_expect_date[1] >= user_birth_date[1] && to_expect_date[0] >= user_birth_date[0]) age++;
+    if (end_user_date[0] >= start_user_date[0]) age++;
     return age;
 }
 
 int main()               
-{ 
-    cout << "Negative years are accepted and represent XXXX(b.C) dates.";
-    cout << "Introduce your birth date: " << endl;
-    cout << "> Birth: " << endl;
-    int *user_birth_date = request_birth_date();
-    cout << "Birth date: " << user_birth_date[0] << "/" << user_birth_date[1] << "/" << user_birth_date[2] << endl;
-    cout << "\n ----------------------" << endl;
-    cout << "Introduce date to calculate age: " << endl;
-    cout << "> Expect age at: " << endl;
-    int *to_expect_date = request_date_to_calc_age(user_birth_date[2]);
-    cout << "Expect date: " << to_expect_date[0] << "/" << to_expect_date[1] << "/" << to_expect_date[2] << endl;
+{
+    int age;
+    int month_diff;
+    int *start_user_date;
+    int *end_user_date;
+    int save_age_for_prompt;
+    cout << "Introduce start date: " << endl;
+    cout << "> Start: " << endl;
+    start_user_date = request_start_date();
 
-    int age = calc_age(user_birth_date, to_expect_date);
+    cout << "\tAge: ";
+    age = cin_number_int();
+    save_age_for_prompt = age;
+    cout << "\n ----------------------" << endl;
+
+
+    cout << "Introduce end date: " << endl;
+    cout << "> End: " << endl;
+    end_user_date = request_date_to_calc_age(start_user_date[1]);
+
+
+    age += calc_age_diff(start_user_date, end_user_date);
+    month_diff = calc_month_diff(start_user_date, end_user_date);
+
 
     cout << endl;
-    cout << "Birth date: " << user_birth_date[0] << "/" << user_birth_date[1] << "/" << user_birth_date[2] << endl;
-    cout << "Expect date: " << to_expect_date[0] << "/" << to_expect_date[1] << "/" << to_expect_date[2] << endl;
-    cout << "Age is: " << age << endl;
+    cout << "Start age: " << save_age_for_prompt << endl;
+    cout << "Start date: " << start_user_date[0] << "/" << start_user_date[1] << endl;
+    cout << "End date: " << end_user_date[0] << "/" << end_user_date[1] << endl;
+    cout << "Years: " << age << endl;
+    cout << "Months: " << month_diff << endl;
     cout << endl;
 
     return 0;
