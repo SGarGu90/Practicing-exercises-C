9face54cce91efb4a025996616dcb1c2305e7f31
SGarGu90 <sgargu90@gmail.com>
2021-01-11 00:31:27 +0100

    Get function from helpers to generate random number by length and use it to generate random vector of numbers provided by user length

--- Practice 2/exercise_3.cpp
+++ Practice 2/exercise_3.cpp
@@ -15,6 +15,32 @@ void print_vector(vector<int> vector_numbers, string text = "Input vector is: ")
   cout << " }" << endl;
 }
 
+string generate_random_number_by_length(int random_number_length)
+{
+    int count_length = random_number_length;
+    int pseudo_random_number;
+    string random_number = "";
+    int position_to_get_from_generated_number;
+    // Don't use srand inside the loop, use it only once, e.g. at the start of main(). And srand() is exactly how you reset this.
+    srand(time(NULL));
+
+    do {
+        // The rand() function does not generate a truly random number; it actually returns the next pseudo-random value
+        // in a sequence of values ranging from 0 to RAND_MAX. You can change the starting point in that sequence using srand().
+        pseudo_random_number = rand();
+        int position_to_get_from_generated_number = 2;
+        // Generates dynamical position to get from pseudo generated rand number
+        // It means the position from each single random number to get from each pseudo random generated number depending if even or odd number or loop
+        if (pseudo_random_number % 2 || count_length % 2) position_to_get_from_generated_number += 2; // even pseudo_random_number, position to get 2 OR  even loop, plus 2 to the position
+        else position_to_get_from_generated_number += 3; // odd pseudo_random_number, position to get 3 || odd loop, plus 3 to the position
+
+        random_number += to_string(pseudo_random_number)[position_to_get_from_generated_number];
+        count_length -= 1;
+    } while (count_length > 0);
+
+    return random_number;
+}
+
 int cin_number_int(int insert_count_number = -1)
 {
   if (insert_count_number != -1) cout << "Insert (int): " << insert_count_number + 1 << endl;
@@ -35,7 +61,7 @@ void request_vector_numbers(vector<int> &vector_numbers) {
 
   for (int number_pos = 0; number_pos < vector_length; number_pos++)
   {
-    new_number = cin_number_int(number_pos);
+    new_number = stoi(generate_random_number_by_length(1));
     vector_numbers.insert(vector_numbers.end(), new_number);
   }
 }
