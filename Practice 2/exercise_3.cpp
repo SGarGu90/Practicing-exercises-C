46dbf7cbb7a265d85e9e39b657f87257086200cf
SGarGu90 <sgargu90@gmail.com>
2021-01-11 09:50:29 +0100

    Create function to generate axis X interval values

--- Practice 2/exercise_3.cpp
+++ Practice 2/exercise_3.cpp
@@ -5,60 +5,50 @@
 
 using namespace std;
 
-void print_vector(vector<int> vector_numbers, string text = "Input vector is: ")
-{
-  if (text != "") cout << text << "{ ";
-  for (int num_pos = 0; num_pos < vector_numbers.size(); num_pos++) {
-    if (num_pos != vector_numbers.size() - 1) cout << vector_numbers[num_pos] << ", ";
-    else cout << vector_numbers[num_pos];
-  }
-  cout << " }" << endl;
-}
-
 string custom_rand_number_from_0_to(int random_value_range_to, bool is_included = true)
 {
-    string random_number = "";
-    int count_length = to_string(random_value_range_to).size();
-    int pseudo_random_number;
-    string pseudo_random_number_str;
-    char random_number_as_char;
-    string random_number_to_check_as_str;
-    int random_number_to_check_as_int;
+  string random_number = "";
+  int count_length = to_string(random_value_range_to).size();
+  int pseudo_random_number;
+  string pseudo_random_number_str;
+  char random_number_as_char;
+  string random_number_to_check_as_str;
+  int random_number_to_check_as_int;
 
-    do {
-        // The rand() function does not generate a truly random number; it actually returns the next pseudo-random value
-        // in a sequence of values ranging from 0 to RAND_MAX. You can change the starting point in that sequence using srand().
-        pseudo_random_number = rand();
-        pseudo_random_number_str = to_string(pseudo_random_number);
-        int position_to_get_from_generated_number = 2;
-        // Generates dynamical position to get from pseudo generated rand number
-        // It means the position from each single random number to get from each pseudo random generated number depending if even or odd number or loop
-        if (pseudo_random_number % 2 || count_length % 2) position_to_get_from_generated_number += 2; // even pseudo_random_number, position to get 2 OR  even loop, plus 2 to the position
-        else position_to_get_from_generated_number += 3; // odd pseudo_random_number, position to get 3 || odd loop, plus 3 to the position
+  do {
+      // The rand() function does not generate a truly random number; it actually returns the next pseudo-random value
+      // in a sequence of values ranging from 0 to RAND_MAX. You can change the starting point in that sequence using srand().
+      pseudo_random_number = rand();
+      pseudo_random_number_str = to_string(pseudo_random_number);
+      int position_to_get_from_generated_number = 2;
+      // Generates dynamical position to get from pseudo generated rand number
+      // It means the position from each single random number to get from each pseudo random generated number depending if even or odd number or loop
+      if (pseudo_random_number % 2 || count_length % 2) position_to_get_from_generated_number += 2; // even pseudo_random_number, position to get 2 OR  even loop, plus 2 to the position
+      else position_to_get_from_generated_number += 3; // odd pseudo_random_number, position to get 3 || odd loop, plus 3 to the position
 
-        random_number_as_char = pseudo_random_number_str[position_to_get_from_generated_number];
-        random_number_to_check_as_str = random_number + random_number_as_char;
-        random_number_to_check_as_int = stoi(random_number_to_check_as_str);
-
-
-        if (is_included) {
-            if (random_number_to_check_as_int >= 0 && random_number_to_check_as_int <= random_value_range_to)
-            {
-                random_number += random_number_as_char;
-                count_length -= 1;
-            }
-        }
-        else {
-            if (random_number_to_check_as_int > 0 && random_number_to_check_as_int < random_value_range_to)
-            {
-                random_number += random_number_as_char;
-                count_length -= 1;
-            }
-        }
-    } while (count_length > 0);
+      random_number_as_char = pseudo_random_number_str[position_to_get_from_generated_number];
+      random_number_to_check_as_str = random_number + random_number_as_char;
+      random_number_to_check_as_int = stoi(random_number_to_check_as_str);
 
 
-    return random_number;
+      if (is_included) {
+          if (random_number_to_check_as_int >= 0 && random_number_to_check_as_int <= random_value_range_to)
+          {
+              random_number += random_number_as_char;
+              count_length -= 1;
+          }
+      }
+      else {
+          if (random_number_to_check_as_int > 0 && random_number_to_check_as_int < random_value_range_to)
+          {
+              random_number += random_number_as_char;
+              count_length -= 1;
+          }
+      }
+  } while (count_length > 0);
+
+
+  return random_number;
 }
 
 int cin_number_int(int insert_count_number = -1)
@@ -66,9 +56,9 @@ int cin_number_int(int insert_count_number = -1)
   if (insert_count_number != -1) cout << "Insert (int): " << insert_count_number + 1 << endl;
   int input;
   while (!(cin >> input)) {
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cout << "Entered value is not a number, please introduce number\n";
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Entered value is not a number, please introduce number\n";
   }
   return input;
 }
@@ -98,6 +88,21 @@ void populate_vector_with_N_custom_random_numbers(vector<int> &vector_numbers) {
   }
 }
 
+void print_vector(vector<int> vector_numbers, string text = "Input vector is: ")
+{
+  if (text != "") cout << text << "{ ";
+  for (int num_pos = 0; num_pos < vector_numbers.size(); num_pos++) {
+    if (num_pos != vector_numbers.size() - 1) cout << vector_numbers[num_pos] << ", ";
+    else cout << vector_numbers[num_pos];
+  }
+  cout << " }" << endl;
+}
+
+void set_x_axis_interval_values(vector<int> &vector_numbers)
+{
+
+}
+
 int main()
 {
   vector<int> vector_numbers;
@@ -105,6 +110,7 @@ int main()
   print_vector(vector_numbers, "Result vector is: ");
 
   vector<int> x_axis_intervals;
+  set_x_axis_interval_values(x_axis_intervals);
 
   vector<int> y_axis_frequencies;
 
