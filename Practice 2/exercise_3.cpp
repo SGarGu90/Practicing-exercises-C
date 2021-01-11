3b12d291a3cd78c246bc67d66a5602745d9d4148
SGarGu90 <sgargu90@gmail.com>
2021-01-11 01:49:55 +0100

    Refactor code to autogenerate vector with random numbers and allow user to define maximum value of vector numbers and total length of vector

--- Practice 2/exercise_3.cpp
+++ Practice 2/exercise_3.cpp
@@ -15,26 +15,48 @@ void print_vector(vector<int> vector_numbers, string text = "Input vector is: ")
   cout << " }" << endl;
 }
 
-string generate_random_number_by_length(int random_number_length)
+string custom_rand_number_from_0_to(int random_value_range_to, bool is_included = true)
 {
-    int count_length = random_number_length;
-    int pseudo_random_number;
     string random_number = "";
-    int position_to_get_from_generated_number;
+    int count_length = to_string(random_value_range_to).size();
+    int pseudo_random_number;
+    string pseudo_random_number_str;
+    char random_number_as_char;
+    string random_number_to_check_as_str;
+    int random_number_to_check_as_int;
 
     do {
         // The rand() function does not generate a truly random number; it actually returns the next pseudo-random value
         // in a sequence of values ranging from 0 to RAND_MAX. You can change the starting point in that sequence using srand().
         pseudo_random_number = rand();
+        pseudo_random_number_str = to_string(pseudo_random_number);
         int position_to_get_from_generated_number = 2;
         // Generates dynamical position to get from pseudo generated rand number
         // It means the position from each single random number to get from each pseudo random generated number depending if even or odd number or loop
         if (pseudo_random_number % 2 || count_length % 2) position_to_get_from_generated_number += 2; // even pseudo_random_number, position to get 2 OR  even loop, plus 2 to the position
         else position_to_get_from_generated_number += 3; // odd pseudo_random_number, position to get 3 || odd loop, plus 3 to the position
 
-        random_number += to_string(pseudo_random_number)[position_to_get_from_generated_number];
-        count_length -= 1;
+        random_number_as_char = pseudo_random_number_str[position_to_get_from_generated_number];
+        random_number_to_check_as_str = random_number + random_number_as_char;
+        random_number_to_check_as_int = stoi(random_number_to_check_as_str);
+
+
+        if (is_included) {
+            if (random_number_to_check_as_int >= 0 && random_number_to_check_as_int <= random_value_range_to)
+            {
+                random_number += random_number_as_char;
+                count_length -= 1;
+            }
+        }
+        else {
+            if (random_number_to_check_as_int > 0 && random_number_to_check_as_int < random_value_range_to)
+            {
+                random_number += random_number_as_char;
+                count_length -= 1;
+            }
+        }
     } while (count_length > 0);
+
 
     return random_number;
 }
@@ -51,25 +73,38 @@ int cin_number_int(int insert_count_number = -1)
   return input;
 }
 
-void request_vector_numbers(vector<int> &vector_numbers) {
+auto get_rand_vector_num_values_by_max_value() {
+  vector<int> vector_numbers;
   int new_number;
 
   cout << "Insert vector length" << endl;
   int vector_length = cin_number_int();
 
+  cout << "Function autogenerate a vector with random numbers from 0 to 100." << endl;
+  cout << "Set new maximum value or 0 for default. Default is 100." << endl;
+  int vector_max_value = cin_number_int();
+  if (vector_max_value == 0) vector_max_value = 100;
+  // Don't use srand inside the loop, use it only once, e.g. at the start of main(). And srand() is exactly how you reset this.
+  srand(time(NULL));
   for (int number_pos = 0; number_pos < vector_length; number_pos++)
   {
-    new_number = stoi(generate_random_number_by_length(1));
+    new_number = stoi(custom_rand_number_from_0_to(vector_max_value));
     vector_numbers.insert(vector_numbers.end(), new_number);
   }
+
+  return vector_numbers;
 }
 
 int main()
 {
-  // Don't use srand inside the loop, use it only once, e.g. at the start of main(). And srand() is exactly how you reset this.
-  srand(time(NULL));
   vector<int> vector_numbers;
-  request_vector_numbers(vector_numbers);
+  vector_numbers = get_rand_vector_num_values_by_max_value();
+
+
+  // cout << "Set Range (X) axis interval: " << endl;
+  // int interval_width = cin_number_int();
+
+
 
   print_vector(vector_numbers, "Result vector is: ");
 
