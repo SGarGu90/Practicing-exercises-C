af89059d2194a554c30b2c4de2c5585fde9df9a5
SGarGu90 <sgargu90@gmail.com>
2021-01-17 00:36:57 +0100

    Create function to input string and request user insert 3 strings

--- Practice 2/exercise_5.cpp
+++ Practice 2/exercise_5.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <limits> // To request strict number in cin (not allowed letters) numeric_limits<streamsize>..
+#include<cmath>
+#include <iomanip> // use set() to generate empty spaces between text
+#include <algorithm>
 
 using namespace std;
 
+string cin_string()
+{
+  string input;
+  cin >> input;
+  return input;
+}
+
 int main()
 {
+  cout << "Insert string (1):  ";
+  string str_input_1 = cin_string();
+  cout << "Insert string (2): ";
+  string str_input_2 = cin_string();
+  cout << "Insert string (3): ";
+  string str_input_3 = cin_string();
+
+
 
   return 0;
 }
